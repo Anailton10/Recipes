8eb7f2ff1bc1f6a31a9896309ebc0120fb34b216
Anailton Silva <francisco.anailton17@gmail.com>
2023-11-05 18:21:42 -0300

    add os caminhos para url do app recipes

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from . import views
+
 
 urlpatterns = [
-    path(),
+    path('', views.home, name='home'),
+    path('contato/', views.contato, name='contato'),
+    
 ]
