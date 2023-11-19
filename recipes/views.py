c46236b53560927a63f204d13d8dc72821c6e06d
Anailton Silva <francisco.anailton17@gmail.com>
2023-11-19 19:46:10 -0300

    Adicionando condicionais nos templates

--- recipes/views.py
+++ recipes/views.py
@@ -21,6 +21,7 @@ def recipe(request, id):
 def category(request, category_id):
     recipes = Recipe.objects.filter(category__id=category_id,
                                     is_published=True).order_by('-id')
-    return render(request, 'recipes/pages/home.html', context={
+    return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{recipes.first().category.name} | Category | '
     })
