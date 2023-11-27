18afa78f81bc2c161748b4c8ba3293086897de7f
Anailton Silva <francisco.anailton17@gmail.com>
2023-11-27 19:46:28 -0300

    teste de status 404 em category e detail

--- recipes/tests/test_recioe_views.py
+++ recipes/tests/test_recioe_views.py
@@ -28,8 +28,20 @@ class RecipeViewsTests(TestCase):
         )
         self.assertIs(view.func, views.category)
 
+    def test_recipe_category_views_returns_404_if_no_recipes(self):
+        response = self.client.get(
+            reverse('recipes-category', kwargs={'category_id': 1})
+        )
+        self.assertEqual(response.status_code, 404)
+
     def test_recipe_detail_view_function_is_correct(self):
         view = resolve(
             reverse('recipes-recipe', kwargs={'id': 1})
         )
         self.assertIs(view.func, views.recipe)
+
+    def test_recipe_detail_views_returns_404_if_no_recipes(self):
+        response = self.client.get(
+            reverse('recipes-recipe', kwargs={'id': 1})
+        )
+        self.assertEqual(response.status_code, 404)
