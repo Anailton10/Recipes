f238bd8da0636c500f44e81c68e556d33a238557
Anailton Silva <francisco.anailton17@gmail.com>
2023-11-27 19:23:13 -0300

    organizando

--- recipes/tests/test_recioe_views.py
+++ recipes/tests/test_recioe_views.py
@@ -8,6 +8,20 @@ class RecipeViewsTests(TestCase):
         view = resolve(reverse('recipes-home'))
         self.assertIs(view.func, views.home)
 
+    def test_recipe_home_views_returns_status_code_200_ok(self):
+        response = self.client.get(reverse('recipes-home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_recipe_home_views_loads_correct_template(self):
+        response = self.client.get(reverse('recipes-home'))
+        self.assertTemplateUsed(response, 'recipes/pages/home.html')
+
+    def test_recipe_home_template_shows_no_recipes_found_if_no_recipes(self):
+        response = self.client.get(reverse('recipes-home'))
+        self.assertIn(
+            '<h1>No recipes found here 🥲</h1>',
+            response.content.decode('utf-8'))
+
     def test_recipe_category_view_function_is_correct(self):
         view = resolve(
             reverse('recipes-category', kwargs={'category_id': 1})
@@ -19,11 +33,3 @@ class RecipeViewsTests(TestCase):
             reverse('recipes-recipe', kwargs={'id': 1})
         )
         self.assertIs(view.func, views.recipe)
-
-    def test_recipe_home_views_returns_status_code_200_ok(self):
-        response = self.client.get(reverse('recipes-home'))
-        self.assertEqual(response.status_code, 200)
-
-    def test_recipe_home_views_loads_correct_template(self):
-        response = self.client.get(reverse('recipes-home'))
-        self.assertTemplateUsed(response, 'recipes/pages/home.html')
