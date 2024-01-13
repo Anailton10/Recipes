299e1bf7fae209e3f842525ec3d6d86a9405fe97
Anailton Silva <francisco.anailton17@gmail.com>
2024-01-12 22:29:22 -0300

    configurando os fomularios com forms

--- authors/forms.py
+++ authors/forms.py
@@ -12,3 +12,43 @@ class RegisterForm(forms.ModelForm):
             'email',
             'password',
         ]
+        # exclude = ['first_name']
+        labels = {
+            'first_name': 'First name',
+            'last_name': 'Last name',
+            'username': 'Username',
+            'email': 'Email',
+            'password': 'Password',
+        }
+
+        help_texts = {
+            'email': 'The e-mail must be valid',
+        }
+
+        error_messages = {
+            'username': {
+                'required': 'This field must not be empty',
+            }
+        }
+
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'placeholder': 'Type your first name here...'
+            }),
+
+            'last_name': forms.TextInput(attrs={
+                'placeholder': 'Type your last name here...'
+            }),
+
+            'username': forms.TextInput(attrs={
+                'placeholder': 'Type your username here...'
+            }),
+
+            'email': forms.EmailInput(attrs={
+                'placeholder': 'Type your e-mail here...'
+            }),
+
+            'password': forms.PasswordInput(attrs={
+                'placeholder': 'Type your password here...'
+            })
+        }
