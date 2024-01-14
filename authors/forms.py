39674b822214cc8e9252c5111ba25f358ad18be4
Anailton Silva <francisco.anailton17@gmail.com>
2024-01-13 21:53:04 -0300

    sobreescrevendo os campos

--- authors/forms.py
+++ authors/forms.py
@@ -2,7 +2,54 @@ from django import forms
 from django.contrib.auth.models import User
 
 
+def add_attr(field, attr_name, attr_new_vall):
+    existing_attr = field.widget.attrs.get(attr_name, '')
+    field.widget.attrs[attr_name] = f'{existing_attr} {attr_new_vall}'.strip
+
+
+def add_placeholder(field, placeholder_val):
+    field.widget.attrs['placeholder'] = placeholder_val
+
+
 class RegisterForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        add_placeholder(
+            field=self.fields['username'], placeholder_val='Your username')
+        add_placeholder(
+            field=self.fields['email'], placeholder_val='Your e-mail')
+        add_placeholder(
+            field=self.fields['first_name'], placeholder_val='Your first name'
+        )
+        add_placeholder(
+            field=self.fields['last_name'], placeholder_val='Your last name'
+        )
+        add_placeholder(
+            field=self.fields['password'], placeholder_val='Your password'
+        )
+
+    password = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={'placeholder': 'Your password'}),
+        error_messages={
+            'required': 'Password must be valid'
+        },
+        help_text=(
+            'Password must have at least one uppercase letter, '
+            'one lowercase letter and one number. The length should be '
+            'at least 8 characters.'
+        )
+    )
+    password2 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Repeat your password'
+        }),
+        error_messages={
+            'required': 'Password must be valid'
+        }
+    )
+
     class Meta:
         model = User
         fields = [
@@ -27,6 +74,10 @@ class RegisterForm(forms.ModelForm):
 
         error_messages = {
             'username': {
+                'required': 'This field must not be empty',
+            },
+
+            'password': {
                 'required': 'This field must not be empty',
             }
         }
